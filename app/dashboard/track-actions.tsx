1e966e364729134cc8a0e89841e12f1b2aeed9c4
rasya <rasyatsaniputra001@gmail.com>
2025-12-06 00:17:00 +0700

    add track actions

---
+++ app/dashboard/track-actions.tsx
@@ -0,0 +1,15 @@
+import { RowDataPacket } from "mysql2";
+import connection from "../lib/db";
+import { Track } from "../lib/models/track-model";
+
+export async function getAllTracks() {
+  try {
+    const [rows] = await connection.execute<RowDataPacket[]>(
+      "SELECT * FROM tracks"
+    );
+
+    return { success: true, religions: rows as Track[] };
+  } catch (err: any) {
+    return { success: false, error: "Gagal mengambil data." };
+  }
+}
