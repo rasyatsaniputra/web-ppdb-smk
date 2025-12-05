7021e0beec97a71f66bd947fe71ba34489f35c4b
rasya <rasyatsaniputra001@gmail.com>
2025-12-06 00:44:49 +0700

    add optional to application model status

---
+++ app/lib/models/application-model.ts
@@ -0,0 +1,17 @@
+export interface Application {
+  id?: number;
+  user_id: number;
+  full_name: string;
+  nisn: string;
+  address: string;
+  phone: string;
+  place_of_birth: string;
+  date_of_birth: string | Date;
+  gender: "M" | "F";
+  previous_school_name: string;
+  registration_date: string | Date;
+  religion_id: number;
+  major_id: number;
+  track_id: number;
+  status?: "pending" | "accepted" | "rejected" | "verified" | "cancelled";
+}
