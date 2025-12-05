1a7af2ca1c08e05bbd100dd8a15f748836bc9636
rasya <rasyatsaniputra001@gmail.com>
2025-12-06 00:16:21 +0700

    add major actions

---
+++ app/dashboard/major-actions.tsx
@@ -0,0 +1,15 @@
+import { RowDataPacket } from "mysql2";
+import connection from "../lib/db";
+import { Major } from "../lib/models/major-model";
+
+export async function getAllMajors() {
+  try {
+    const [rows] = await connection.execute<RowDataPacket[]>(
+      "SELECT * FROM majors"
+    );
+
+    return { success: true, religions: rows as Major[] };
+  } catch (err: any) {
+    return { success: false, error: "Gagal mengambil data." };
+  }
+}
