c872ca1b7dff464ba449839046bd342b7ccdd487
rasya <rasyatsaniputra001@gmail.com>
2025-12-06 00:14:24 +0700

    add religion actions

---
+++ app/dashboard/religion-actions.tsx
@@ -0,0 +1,15 @@
+import { RowDataPacket } from "mysql2";
+import connection from "../lib/db";
+import { Religion } from "../lib/models/religion-model";
+
+export async function getAllReligions() {
+  try {
+    const [rows] = await connection.execute<RowDataPacket[]>(
+      "SELECT * FROM religions"
+    );
+
+    return { success: true, religions: rows as Religion[] };
+  } catch (err: any) {
+    return { success: false, error: "Gagal mengambil data." };
+  }
+}
